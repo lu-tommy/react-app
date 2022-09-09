5ad41d0c9df5dddfb197605ae9440e5dfd21d392
tlus3r <tommylu3h@gmail.com>
2022-09-08 21:33:25 -0400

    g

--- react-app/src/marvin.js
+++ react-app/src/marvin.js
@@ -6,29 +6,6 @@ const aboutMarvin ='Marvin is a really cool bot'
 
 
 
-
-function openAI(){
-  const { Configuration, OpenAIApi } = require("openai");
-
-  const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-  });
-  const openai = new OpenAIApi(configuration);
-
-  const response = await openai.createCompletion({
-    model: "text-davinci-002",
-    prompt: "",
-    temperature: 0.7,
-    max_tokens: 256,
-    top_p: 1,
-    frequency_penalty: 0,
-    presence_penalty: 0,
-  });
-  }
-
-
-
-
 function Marvin() {
     return (
      <>

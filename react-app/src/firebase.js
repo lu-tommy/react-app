ca28ec98a0408754b64c92a32755199dc4bc1c30
tlus3r <tommylu3h@gmail.com>
2022-09-12 17:49:56 -0400

    remove sensitive data

--- react-app/src/firebase.js
+++ react-app/src/firebase.js
@@ -6,14 +6,6 @@ import { getDatabase } from "firebase/database";
 // See: https://firebase.google.com/docs/web/learn-more#config-object
 const firebaseConfig = {
 
-  apiKey: "AIzaSyCYa84pxeWYn48vsUDK3NZAi7u0acWVK58",
-  authDomain: "marvinchat-537fe.firebaseapp.com",
-  databaseURL: "https://marvinchat-537fe-default-rtdb.firebaseio.com",
-  projectId: "marvinchat-537fe",
-  storageBucket: "marvinchat-537fe.appspot.com",
-  messagingSenderId: "740543180964",
-  appId: "1:740543180964:web:c57d1a25bd5e72e1c37631",
-  measurementId: "G-YEW53LHBKR"
 };
 
 // Initialize Firebase
